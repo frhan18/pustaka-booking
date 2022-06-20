3b31f76db54c6f0ec1c03a33cb5f5e2e1550c90d
Farhan <farhan18apr02@gmail.com>
2022-06-20 17:36:20 +0700

    update tampilan login & register

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,48 +1,40 @@
-<!-- Outer Row -->
 <div class="row justify-content-center">
+  <div class="col-lg-7 col-md-7 col-sm-10">
+    <div class="login-container">
 
-  <div class="col-xl-7 col-sm-10">
-
-    <div class="card o-hidden border-0 shadow-lg my-5">
-      <div class="card-body p-0">
-        <!-- Nested Row within Card Body -->
-        <div class="row">
-          <div class="col">
-            <div class="p-5">
-              <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-              </div>
-
+      <div class="row align-content-center justify-content-center">
+        <div class="col-lg-10 col-md-10">
+          <div class="login-body__content">
+            <div class="login-body__info">
+              <h3 class="mb-0 text-capitalize ">Selamat datang di pustaka booking </h3>
+              <p>Akses buku lebih mudah dan cepat dengan menggunakan aplikasi <strong>pustaka booking.</strong></p>
 
               <?php if ($this->session->flashdata('message_success')) : ?>
-                <div class="notification-fb">
-                  <div class="alert alert-success  alert-dismissible fade show" role="alert">
-                    <?= $this->session->flashdata('message_success'); ?>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                  <?= $this->session->flashdata('message_success'); ?>
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                 </div>
               <?php endif; ?>
-
-
               <?php if ($this->session->flashdata('message_error')) : ?>
-                <div class="notification-fb">
-                  <div class="alert alert-danger  alert-dismissible fade show" role="alert">
-                    <?= $this->session->flashdata('message_error'); ?>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                  <?= $this->session->flashdata('message_error'); ?>
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                 </div>
               <?php endif; ?>
 
-
-
-              <!-- <form class="user"> -->
+              <div class="alert alert-warning mt-2" role="alert">
+                <h4 class="alert-heading">Member Login!</h4>
+                <p>Gunakan Alamat email yang sudah teregistrasi di sistem. <br> Apabila akun tidak aktif segera verifikasi akun tesebut.</p>
+              </div>
+            </div>
+            <div class="login-body__form">
               <?= form_open('auth', 'class="user"'); ?>
               <div class="form-group">
-                <input type="text" name="email" value="<?= set_value('email'); ?>" class="form-control  <?= form_error('email') ? 'is-invalid' : ''; ?>  form-control-user" placeholder="Alamat email" required autofocus>
+                <input type="text" name="email" value="<?= set_value('email'); ?>" class="form-control  <?= form_error('email') ? 'is-invalid' : ''; ?>  form-control-user" placeholder="Alamat email" required>
                 <div class="invalid-feedback ml-2"><?= form_error('email'); ?></div>
               </div>
               <div class="form-group">
@@ -61,12 +53,17 @@
               <div class="text-center">
                 <a class="small" href="<?= site_url('auth/register'); ?>">Belum mempunyai akun? buat sekarang</a>
               </div>
+
             </div>
+
           </div>
         </div>
       </div>
+
     </div>
 
   </div>
+
+
 
 </div>
--- application/views/template/frontend/header.php
+++ application/views/template/frontend/header.php
@@ -13,9 +13,10 @@
   <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/'); ?>css/style.css" rel="stylesheet">
 
 </head>
 
-<body class="bg-gradient-primary">
+<body id="bg-container__auth">
 
   <div class="container">
